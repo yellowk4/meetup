be5e6f498d7c3e286038060ffc88384140da6830
yellowkk <yellowk4@hunet.co.kr>
2024-08-21 17:48:40 +0900

    수정

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -8,29 +8,16 @@ async function handler(req, res) {
     const data = req.body;
     const { title, image, address, description } = data;
 
-    let client;
+    const client = await MongoClient.connect(
+      'mongodb+srv://admin:5ZMFz9r8zTvoJEKs@cluster0.d5sec.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    );
+    const db = client.db();
+    const meetupsCollection = db.collection('meetups'); // collection 이름
+    const result = await meetupsCollection.insertOne(data); // data = { title, image, address, description }
+    console.log(result);
+    client.close();
 
-    try {
-      client = await MongoClient.connect(
-        'mongodb+srv://admin:5ZMFz9r8zTvoJEKs@cluster0.d5sec.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-        { useNewUrlParser: true, serverSelectionTimeoutMS: 30000 } // 30 seconds timeout
-      );
-      const db = client.db();
-      const meetupsCollection = db.collection('meetups');
-      const result = await meetupsCollection.insertOne(data);
-      console.log('Insertion result:', result);
-
-      res.status(201).json({ message: 'Meetup inserted!' });
-    } catch (error) {
-      console.error('Error connecting to MongoDB or inserting data:', error);
-      res.status(500).json({ message: 'Failed to insert meetup' });
-    } finally {
-      if (client) {
-        client.close();
-      }
-    }
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
 
